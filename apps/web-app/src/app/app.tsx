a631c2201c55fc46b1d74c89d4a9b6dd3ad45e07
tdoval <thiagodovalaraujo@gmail.com>
2022-10-11 15:18:45 -0300

    error launching apollo - refactoring

--- apps/web-app/src/app/app.tsx
+++ apps/web-app/src/app/app.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Message } from '@todo-starter/api-interfaces';
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
 
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api')
+  //     .then((r) => r.json())
+  //     .then(setMessage);
+  // }, []);
 
   return (
     <>
@@ -20,7 +18,7 @@ export const App = () => {
           alt="Nx - Smart, Fast and Extensible Build System"
         />
       </div>
-      <div>{m.message}</div>
+      <div>MyTeeext</div>
     </>
   );
 };
